5b730e7bab59e24c06bd07ac30bcb567988c5ce4
Yuriy Svichenskiy <yuriy.svichenskiy@gmail.com>
2026-04-18 21:55:12 +0300

    Added basic sign-up and sign-in endpoints.

---
+++ src/iam/authentication/authentication.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
+
+@Controller('authentication')
+export class AuthenticationController {
+    constructor (
+        private readonly authService: AuthenticationService
+    ) {}
+
+    @Post('sign-up')
+    singUp (@Body() singUpDto: SignUpDto) {
+        return this.authService.signUp(singUpDto);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('sign-in')
+    singIn (@Body() singInDto: SignInDto) {
+        return this.authService.signIn(singInDto);
+    }
+}
